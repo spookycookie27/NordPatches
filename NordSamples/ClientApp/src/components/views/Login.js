7d9a37293c18c59c23dadc664fb99f83ed1c8b82
spookycookie <sp.cooke@me.com>
2019-11-26 08:45:50 +0000

    Added login page and click event

--- NordSamples/ClientApp/src/components/views/Login.js
+++ NordSamples/ClientApp/src/components/views/Login.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
@@ -12,13 +10,15 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { setToken, signOut } from '../../services/Auth';
+import RestUtilities from '../../services/RestUtilities';
 
 function Copyright() {
   return (
     <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
       <Link color='inherit' href='https://material-ui.com/'>
-        Your Website
+        Nord User Patches
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -58,6 +58,24 @@ const useStyles = makeStyles(theme => ({
 
 export default function SignInSide() {
   const classes = useStyles();
+  const [email, setEmail] = useState('sp.cooke@me.com');
+  const [password, setPassword] = useState('Passw0rd1!');
+  const [error, setError] = useState(false);
+  console.log(error);
+  function handleLoginClick() {
+    console.log('clicked');
+    const url = '/api/auth/login';
+    const data = { email: email, password: password };
+    RestUtilities.post(url, data).then(async response => {
+      if (response.ok) {
+        setToken(response.content.token);
+        //await this.props.setUser(response.content.user);
+        //this.props.history.push('/');
+      } else {
+        setError(true);
+      }
+    });
+  }
 
   return (
     <Grid container component='main' className={classes.root}>
@@ -72,9 +90,22 @@ export default function SignInSide() {
             Sign in
           </Typography>
           <form className={classes.form} noValidate>
-            <TextField variant='outlined' margin='normal' required fullWidth id='email' label='Email Address' name='email' autoComplete='email' autoFocus />
             <TextField
               variant='outlined'
+              value={email}
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+              onChange={event => setEmail(event.target.value)}
+            />
+            <TextField
+              variant='outlined'
+              value={password}
               margin='normal'
               required
               fullWidth
@@ -83,19 +114,19 @@ export default function SignInSide() {
               type='password'
               id='password'
               autoComplete='current-password'
+              onChange={event => setPassword(event.target.value)}
             />
-            <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me' />
-            <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+            <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit} onClick={() => handleLoginClick()}>
               Sign In
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href='#' variant='body2'>
+                <Link href='/Identity/Account/ForgotPassword' variant='body2'>
                   Forgot password?
                 </Link>
               </Grid>
               <Grid item>
-                <Link href='#' variant='body2'>
+                <Link href='/Identity/Account/Register' variant='body2'>
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
