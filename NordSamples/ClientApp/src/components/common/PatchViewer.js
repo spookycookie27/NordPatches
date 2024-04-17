c66c255ecf08fed2a534e2e03817db414a85bb65
Steve Cooke <spookycookie27@gmail.com>
2024-04-18 00:28:22 +0100

    removed unknown rule

--- NordSamples/ClientApp/src/components/common/PatchViewer.js
+++ NordSamples/ClientApp/src/components/common/PatchViewer.js
@@ -73,7 +73,6 @@ const PatchViewer = (props) => {
       refreshData();
     };
     getData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const addRating = async (newValue) => {
